ebbaefd3c06d52a58243177c35f57fda1f8c08ac
yhyzgn <1436433211@qq.com>
2017-08-11 14:22:56 +0800

    修复SystemUtils中安装apk方法在Android7.0+奔溃的bug。

---
+++ utils/src/main/java/com/yhy/utils/provider/AUFileProvider.java
@@ -0,0 +1,9 @@
+package com.yhy.utils.provider;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by 颜洪毅 on 2017/8/11 14:12.
+ */
+public class AUFileProvider extends FileProvider {
+}
